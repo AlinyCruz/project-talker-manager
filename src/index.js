864d6f3e87a0bd28c09b0192c05d81e171c2b341
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-04-14 18:28:20 -0300

    Requisito 01

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -14,4 +15,30 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+//----------- Meu código começa a partir daqui ----------------//
+
+async function getTalkers() {
+  const arrayTalker = path.resolve(__dirname,'talker.json');
+  console.log(arrayTalker);
+  try {
+    const readTalker = await fs.readFile(arrayTalker, 'utf-8');
+    const talkers = JSON.parse(readTalker);
+    console.log(talkers);
+    return talkers;
+  }
+  catch(error) {
+    return [];
+  }
+}
+getTalkers();
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talkers2 = await getTalkers();
+    res.status(200).json(talkers2);
+  } catch (error) {
+    res.status(200).send([]);
+  }
 });
